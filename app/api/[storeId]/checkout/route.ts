6dc7561df69b70f176df09f5d49f9d3146cf7766
Konstantin Nikolaev <you@example.com>
2023-10-10 15:43:57 -0700

    Stripe Set Up

---
+++ app/api/[storeId]/checkout/route.ts
@@ -0,0 +1,93 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+
+import { stripe } from "@/lib/stripe";
+import prismadb from "@/lib/prismadb";
+
+export type CartItem = {
+  id: string;
+  quantity: number;
+};
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Origin, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  const { cartItems }: { cartItems: CartItem[] } = await req.json();
+
+  if (cartItems.length === 0)
+    return new NextResponse("Cart Items are required", { status: 400 });
+
+  const productIds = cartItems.map((item) => item.id);
+
+  const products = await prismadb.product.findMany({
+    where: {
+      id: {
+        in: productIds,
+      },
+    },
+  });
+
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+  products.forEach((product) => {
+    const productQuantity = cartItems.find((item) => item.id === product.id);
+    line_items.push({
+      quantity: productQuantity?.quantity,
+      price_data: {
+        currency: "USD",
+        product_data: {
+          name: product.name,
+        },
+        unit_amount: product.price.toNumber() * 100,
+      },
+    });
+  });
+
+  const order = await prismadb.order.create({
+    data: {
+      storeId: params.storeId,
+      isPaid: false,
+      orderItems: {
+        create: productIds.map((productId: string) => ({
+          product: {
+            connect: {
+              id: productId,
+            },
+          },
+        })),
+      },
+    },
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: "payment",
+    billing_address_collection: "required",
+    phone_number_collection: {
+      enabled: true,
+    },
+    success_url: `${process.env.FRONTEND_STORE_URL}?success=1`,
+    cancel_url: `${process.env.FRONTEND_STORE_URL}?canceled=1`,
+    metadata: {
+      orderId: order.id,
+    },
+  });
+
+  return NextResponse.json(
+    { url: session.url },
+    {
+      headers: corsHeaders,
+    }
+  );
+}
